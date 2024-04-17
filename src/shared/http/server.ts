84bb70089eb56462aa46006ffea9c3def176a24e
Douglas Trindade <douglastrindade22@hotmail.com>
2024-04-17 02:14:44 -0300

    configuration database connect with typeorm

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,4 +1,5 @@
-import express from 'express';
+import 'reflect-metadata';
+import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import { routes } from './routes';
 import { AppError } from '../errors/AppError';
@@ -11,12 +12,7 @@ app.use(express.json());
 app.use(routes);
 
 app.use(
-  (
-    error: Error,
-    request: express.Request,
-    response: express.Response,
-    next: express.NextFunction,
-  ) => {
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
     if (error instanceof AppError) {
       response.status(error.statusCode).json({
         status: 'error',
